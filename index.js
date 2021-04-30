786554d60dabb494ba9e2425471247b1adecbe8c
Tales Salinas Rozo <talesrozo@hotmail.com>
2021-04-29 21:44:47 -0400

    Connected to the API

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const exphbs = require('express-handlebars')
+const request = require('request')
 
 const PORT = process.env.PORT || 5000
+
+// API KEY pk_1bb06023916a4c8093d29b42fb4518a3
+// Create call_api function
+// company news/last/10
+function call_api(finishedAPI) {
+    request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_1bb06023916a4c8093d29b42fb4518a3',{json: true}, (err, res, body) => {
+    if (err) {
+        return console.log(err)
+    }
+    if (res.statusCode === 200) {
+        finishedAPI(body)
+    }
+})
+}
 
 // Set Handlebars Middleware
 app.engine('handlebars', exphbs())
@@ -13,8 +28,10 @@ app.set('view engine', 'handlebars')
 const testphrase = "Hi, my name is Tales."
 
 app.get('/', function(req, res) {
-    res.render('home', {
-        test: testphrase
+    call_api(function(doneAPI) {
+        res.render('home', {
+            stock: doneAPI
+        })
     })
 })
 
