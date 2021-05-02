4512ea732b06d93374fbf39d3197ee8f68b05618
Tales Salinas Rozo <talesrozo@hotmail.com>
2021-05-01 22:10:42 -0400

    The form is working fine

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const app = express()
 const path = require('path')
 const exphbs = require('express-handlebars')
 const request = require('request')
+const bodyParser = require('body-parser')
 
 const PORT = process.env.PORT || 5000
 
+// Use body parser middleware
+app.use(bodyParser.urlencoded({extended: false}))
+
 // API KEY pk_1bb06023916a4c8093d29b42fb4518a3
 // Create call_api function
-function call_api(finishedAPI) {
-    request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_1bb06023916a4c8093d29b42fb4518a3',{json: true}, (err, res, body) => {
+function call_api(finishedAPI, ticker) {
+    request(`https://cloud.iexapis.com/stable/stock/${ticker}/quote?token=pk_1bb06023916a4c8093d29b42fb4518a3`,{json: true}, (err, res, body) => {
     if (err) {
         return console.log(err)
     }
@@ -23,7 +27,7 @@ function call_api(finishedAPI) {
 app.engine('handlebars', exphbs())
 app.set('view engine', 'handlebars')
 
-// Set Handlebars Routes
+// Set Handlebars Index Route
 const testphrase = "Hi, my name is Tales."
 
 app.get('/', function(req, res) {
@@ -31,7 +35,17 @@ app.get('/', function(req, res) {
         res.render('home', {
             stock: doneAPI
         })
-    })
+    }, "tsla")
+})
+
+// Set Handlebars Index POST Route
+app.post('/', function(req, res) {
+    call_api(function(doneAPI) {
+        /* posted_stuff = req.body.stock_ticker */
+        res.render('home', {
+            stock: doneAPI,
+        })
+    }, req.body.stock_ticker)
 })
 
 // Create about page route
